7770d0f8e39ba07de613c5b67b0892e02e8f32c4
Joe Cooper <joe@virtualmin.com>
2016-07-03 19:37:57 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::Route53 - It's new $module
+
+# SYNOPSIS
+
+    use App::Route53;
+
+# DESCRIPTION
+
+App::Route53 is ...
+
+# LICENSE
+
+Copyright (C) Joe Cooper.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Joe Cooper <joe@virtualmin.com>
---
+++ lib/App/Route53.pm
@@ -0,0 +1,39 @@
+package App::Route53;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Route53 - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::Route53;
+
+=head1 DESCRIPTION
+
+App::Route53 is ...
+
+=head1 LICENSE
+
+Copyright (C) Joe Cooper.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Joe Cooper E<lt>joe@virtualmin.comE<gt>
+
+=cut
+
